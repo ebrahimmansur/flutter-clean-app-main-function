9c2050baf3fd625b5e7a6c139bc11801db580bb4
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2022-11-11 14:43:20 +0200

    👷 Add : app configuration builder

--- demo_app/lib/src/app/app_host.dart
+++ demo_app/lib/src/app/app_host.dart
@@ -1,1 +1,7 @@
+import 'dart:async';
 
+abstract class IAppHostConfigurationBuilder {
+  ///Builds and runs the pre-configurations before the runapp() function.
+  IAppHostConfigurationBuilder._();
+  Future<void> run();
+}
