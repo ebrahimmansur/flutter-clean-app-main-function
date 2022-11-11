57eace444ab110d21eea627572fa331231871459
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2022-11-11 20:15:20 +0200

    👷 Add : app builder

--- demo_app/lib/src/app/app_host.dart
+++ demo_app/lib/src/app/app_host.dart
@@ -1,7 +1,38 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
+
 ///Builds and runs the pre-configurations before the runapp() function.
 abstract class IAppHostConfigurationBuilder {
   IAppHostConfigurationBuilder._();
   Future<void> run();
+}
+
+class _AppBuilder {
+  Widget? _app;
+  final List<IAppHostConfigurationBuilder> _configurations = [];
+  static _AppBuilder? _instance;
+  _AppBuilder._();
+
+  factory _AppBuilder.instance() {
+    if (_instance == null) {
+      _instance = _AppBuilder._();
+      return _instance!;
+    }
+
+    return _instance!;
+  }
+
+  void addApp(Widget app) => _app = app;
+  void addConfig(IAppHostConfigurationBuilder config) =>
+      _configurations.add(config);
+
+  Future<void> build() async {
+    await Future.forEach<IAppHostConfigurationBuilder>(_configurations,
+        (item) async {
+      await item.run();
+    });
+
+    runApp(_app!);
+  }
 }
